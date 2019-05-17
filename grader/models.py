3c0e5db5b501bc2717f095a4f2955324c84386a0
gundamMC <gundammc@qq.com>
2019-05-17 11:19:23 -0700

    Add new django models and improve overall strucutre

--- grader/models.py
+++ grader/models.py
@@ -1,17 +1,40 @@
 import subprocess
 import os
 import json
+import datetime
 
 from shutil import copy
 
 from django.db import models
 
+from users.models import Student
+
 # Create your models here.
+
+
+class Class(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Unit(models.Model):
+    parent_class = models.ForeignKey(Class, on_delete=models.CASCADE)
+
+
+class Assignment(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField(blank=True, null=True)
+
+    unit = models.ForeignKey(Unit, on_delete=models.SET_NULL, default=None, blank=True, null=True)
+    parent_class = models.ForeignKey(Class, on_delete=models.CASCADE)
+    due_date = models.DateTimeField()
 
 
 class Submission(models.Model):
     total_points = models.IntegerField(default=None, blank=True, null=True)
     points = models.IntegerField(default=None, blank=True, null=True)
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    submitted_date = models.DateTimeField()
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
 
     def grade(self):
         if self.points is None:
@@ -60,13 +83,25 @@ class Submission(models.Model):
 
             # self.points = output['points']
             # self.total_points = output['total']
+            self.submitted_date = datetime.datetime.now()
 
         return self.points
 
 
+def content_file_name(instance, filename):
+    now = datetime.datetime.now()
+    return '/'.join(['uploads',
+                     instance.submission.student.student_id,
+                     now.year,
+                     now.month,
+                     now.day,
+                     instance.submission.id,
+                     filename
+                     ])
+
+
 class FileUpload(models.Model):
-    file = models.FileField(upload_to='uploads/%Y/%m/%d/')
+    file = models.FileField(upload_to=content_file_name)
     original_filename = models.CharField(max_length=128)
     upload_date = models.DateTimeField(auto_now_add=True)
     submission = models.ForeignKey(Submission, on_delete=models.CASCADE)
-
