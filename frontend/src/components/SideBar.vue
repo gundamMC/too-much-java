086e1bcbdb368ecfa2b15c9dada7290cbfae7d8f
gundamMC <gundammc@qq.com>
2019-05-23 17:24:59 -0700

    Format sidebar indent

--- frontend/src/components/SideBar.vue
+++ frontend/src/components/SideBar.vue
@@ -1,48 +1,48 @@
 <template>
-  <div>
-<!--    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">-->
-<!--      <el-radio-button :label="false">expand</el-radio-button>-->
-<!--      <el-radio-button :label="true">collapse</el-radio-button>-->
-<!--    </el-radio-group>-->
-    <el-menu default-active="2" class="el-menu-vertical-demo">
-      <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span slot="title">Navigator One</span>
-        </template>
-        <el-menu-item-group>
-          <span slot="title">Group One</span>
-          <el-menu-item index="1-1">item one</el-menu-item>
-          <el-menu-item index="1-2">item two</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="Group Two">
-          <el-menu-item index="1-3">item three</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <span slot="title">item four</span>
-          <el-menu-item index="1-4-1">item one</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">Navigator Two</span>
-      </el-menu-item>
-      <el-menu-item index="3" disabled>
-        <i class="el-icon-document"></i>
-        <span slot="title">Navigator Three</span>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <span slot="title">Navigator Four</span>
-      </el-menu-item>
-    </el-menu>
-  </div>
+    <div>
+        <!--    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">-->
+        <!--      <el-radio-button :label="false">expand</el-radio-button>-->
+        <!--      <el-radio-button :label="true">collapse</el-radio-button>-->
+        <!--    </el-radio-group>-->
+        <el-menu default-active="2" class="el-menu-vertical-demo">
+            <el-menu-item index="1">
+                <i class="el-icon-menu"></i>
+                <span slot="title">Dashboard</span>
+            </el-menu-item>
+            <el-submenu index="2">
+                <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span slot="title">Navigator One</span>
+                </template>
+                <el-menu-item-group>
+                    <span slot="title">Group One</span>
+                    <el-menu-item index="2-1">item one</el-menu-item>
+                    <el-menu-item index="2-2">item two</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="Group Two">
+                    <el-menu-item index="2-3">item three</el-menu-item>
+                </el-menu-item-group>
+                <el-submenu index="2-4">
+                    <span slot="title">item four</span>
+                    <el-menu-item index="2-4-1">item one</el-menu-item>
+                </el-submenu>
+            </el-submenu>
+            <el-menu-item index="3" disabled>
+                <i class="el-icon-document"></i>
+                <span slot="title">Navigator Three</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+                <i class="el-icon-setting"></i>
+                <span slot="title">Navigator Four</span>
+            </el-menu-item>
+        </el-menu>
+    </div>
 </template>
 
 <script>
     export default {
         name: "SideBar",
-        data(){
+        data() {
             return {
                 isCollapse: true
             }
@@ -51,8 +51,8 @@
 </script>
 
 <style scoped>
-  .el-menu-vertical-demo:not(.el-menu--collapse) {
-    width: 200px;
-    min-height: 400px;
-  }
+    .el-menu-vertical-demo:not(.el-menu--collapse) {
+        width: 200px;
+        min-height: 400px;
+    }
 </style>
