518630239257b5ba79d08d604abbd992891cd8c1
gundamMC <gundammc@qq.com>
2019-08-16 16:50:59 -0700

    Add assignment dropoff page

--- grader/urls.py
+++ grader/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from .router import router
+from . import views
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 urlpatterns = [
@@ -9,5 +10,6 @@ urlpatterns = [
     path(r'api/', include(router.urls)),
     path(r'', TemplateView.as_view(template_name="index.html"), name="app"),
     # path(r'<path:resource>', TemplateView.as_view(template_name="index.html"), name="app"),
+    path(r'dropoff/', views.dropoff, name='dropoff'),
     re_path(r'^(?P<path>.*)/$', TemplateView.as_view(template_name="index.html")),
 ]
--- grader/views.py
+++ grader/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def dropoff(request):
+    if 'file' not in request.FILES:
+        # no file uploaded, show default page
+        return render(request, '../templates/assignment_dropoff.html')
+    else:
+        f = request.FILES['file'] # here you get the files needed
+        print(f.name)
+        return render(request, '../templates/assignment_dropoff.html', {'name': f.name})
